d4eb746e783a2b6a3f8be3a3a773b5bd9965d20a
Mythical-Github <MythicalData@gmail.com>
2025-01-28 06:22:55 -0500

    Update wrappers.py

--- src/tackle/wrappers.py
+++ src/tackle/wrappers.py
@@ -1,3 +1,25 @@
+import os
+import sys
+from tackle import file_io
 
-def generate_wrapper():
-    return
+
+def get_wrapper_location(wrapper_name: str) -> str:
+    return os.path.normpath(f'{file_io.SCRIPT_DIR}/dist/{wrapper_name}.bat')
+
+
+def generate_wrapper(wrapper_name: str):
+    args = sys.argv[:]
+
+    if "--wrapper_name" in args:
+        index = args.index("--wrapper_name")
+        args.pop(index)
+        args.pop(index)
+
+    content = ' '.join(args)
+
+    wrapper_path = get_wrapper_location(wrapper_name)
+
+    os.makedirs(os.path.dirname(wrapper_path), exist_ok=True)
+
+    with open(wrapper_path, 'w') as f:
+        f.write(content)
